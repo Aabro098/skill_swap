9c932dc97f121fbde96838e3ba38de9b1ca6dc66
Arbin Shrestha <arbinstha71@gmail.com>
2026-02-13 19:05:54 +0545

    Hosted the server

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -1,13 +1,13 @@
 class UrlStrings {
-  static const String baseUrl = 'http://10.0.2.2:5001/';
+  static const String baseUrl = 'https://skill-swap-server-1y5u.onrender.com';
 
   static const String googleAuth = '/auth/google';
 
-  static const String auth = 'api/auth/';
-  static const String user = 'api/user/';
-  static const String completeProfile = 'api/user/complete-profile';
+  static const String auth = '/api/auth/';
+  static const String user = '/api/user/';
+  static const String completeProfile = '/api/user/complete-profile';
 
-  static const String me = 'api/user/me';
+  static const String me = '/api/user/me';
 
   static const String recommendedUsers = '/api/user/predict-users';
   static const String sendRequest = '/api/friends/request';
